f4188179517250a9a40f54a64437055a83311806
Jack Ploetz <jwploetz@gmail.com>
2021-11-05 22:07:20 -0400

    Rename donate to gardenting, added to nav bar

--- js/nav_bar.js
+++ js/nav_bar.js
@@ -2,7 +2,7 @@ directoryDepth = ".".repeat(window.location.href.replace(/[^/A]/g, "").length - 
 
 var menuStr =
    `<li><a href="index.html">Home</a></li>
-    <li><a href="donate.html">Donate</a></li>
+    <li><a href="gardening.html">Lawn Care Fundraiser</a></li>
     <li><a href="contact.html">Contact</a></li>`;
 
 var menuList = document.getElementById("menuBar");
