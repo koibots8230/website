3edcfaf0b08e3f3f2197380b8587282a70e4fad8
AntonioAbela <antonioabela3609@gmail.com>
2021-11-08 13:17:22 -0500

    shortened to lawn care

--- js/nav_bar.js
+++ js/nav_bar.js
@@ -2,7 +2,7 @@ directoryDepth = ".".repeat(window.location.href.replace(/[^/A]/g, "").length - 
 
 var menuStr =
    `<li><a href="index.html">Home</a></li>
-    <li><a href="gardening.html">Lawn Care Fundraiser</a></li>
+    <li><a href="gardening.html">Lawn Care</a></li>
     <li><a href="contact.html">Contact</a></li>`;
 
 var menuList = document.getElementById("menuBar");
