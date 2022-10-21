8946799159830862b73292b355b48d081c6953e7
jwploetz <66195076+jwploetz@users.noreply.github.com>
2022-10-20 20:49:48 -0400

    Added ChedderUp to nav bar

--- js/nav_bar.js
+++ js/nav_bar.js
@@ -3,6 +3,7 @@ directoryDepth = ".".repeat(window.location.href.replace(/[^/A]/g, "").length - 
 var menuStr =
    `<li><a href="index.html">About Us</a></li>
     <li><a href="resources.html">Robotics Resources</a></li>
+    <li><a href="https://koibots8230.cheddarup.com/" target="_blank">Donate<a/></li>
     <li><a href="contact.html">Contact</a></li>`;
 
 var menuList = document.getElementById("menuBar");
